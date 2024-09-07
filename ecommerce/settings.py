88f37d43a2bed7093dcd65df36c584996c80afca
muskan917 <138205826+muskan917@users.noreply.github.com>
2024-09-07 18:44:02 +0530

    Create settings.py

---
+++ ecommerce/settings.py
@@ -0,0 +1,11 @@
+INSTALLED_APPS = [
+    ...
+    'products',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
